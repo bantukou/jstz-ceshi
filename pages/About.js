2c119685db80b4c7782c21ee6bdc7cbc76e93460
bantukou <85782737+bantukou@users.noreply.github.com>
2022-09-16 14:36:09 +0800

    Update About.js

--- pages/About.js
+++ pages/About.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 export default function About() {
   return <div>
     <Link href="https://baidu.com">
-      <a>About Page</a>
+      
     </Link>
-    <p>Hello Next.js</p>
+    
   </div>
    
 }
