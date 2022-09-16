51f061f12526bc6100ade7fe2df8d1d6a65cc2fc
bantukou <85782737+bantukou@users.noreply.github.com>
2022-09-16 15:31:48 +0800

    Create e73752.js

---
+++ pages/e73752.js
@@ -0,0 +1,10 @@
+import {useEffect} from 'react'
+export default () => {
+  useEffect(() => {
+        window.location.assign('https://168015cc.com')
+    })
+    return(
+        <>
+        </>
+    )
+}
